52f191f6bb72c59205d80178d03d4aeb8d104fa3
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2024-03-03 14:18:26 -0700

    Fix EOF

--- LUTHOR/src/index.ts
+++ LUTHOR/src/index.ts
@@ -10,13 +10,11 @@
         const [uFile,srcFile,outFile] = system.args.slice(1);
         const language: LanguageDefinition = readLanguageDefinition(uFile); 
 
-        console.log(language);
-
         const inStream = await system.createFileReadStream(srcFile);
         const outStream = await system.createFileWriteStream(outFile)
         const tape: Tape<char> = new Tape<char>(() => inStream.read(1) as char);
 
-        let byte: char;
+        let byte: char | undefined;
 
         let matchers: TokenMatcher[] = language.createMatchers();
         let bestMatch: [TokenType,number] | null = null;
@@ -24,19 +22,18 @@
         let col = 1;
         let bytes: char[] = []
 
-        while(byte = tape.next()) {
-            bytes.push(byte);
+        while(byte = tape.next() || bytes.length) {
+            if(byte) {
+                bytes.push(byte);
+                matchers.forEach(matcher => matcher.accept(byte!));
+            }
             
-            matchers.forEach(matcher => matcher.accept(byte));
-
             const matcher = matchers.find(matcher => matcher.isComplete()) ?? null;
 
-            if(matchers.every(matcher => matcher.isFailed())) {
-                // console.log(tape.toString())
+            if(matchers.every(matcher => matcher.isFailed()) || !byte) {
                 if(!bestMatch) {
                     throw new Error('Language matched nothing!');
                 }
-                // console.log(`${bestMatch[0].name} --- ` + matchers.map(x=>`${x.getType().name}:${x.state}`).join(', '))
                 outStream.write(`${bestMatch[0].name} ${alphaEncode(bestMatch[0].value ?? bytes.slice(0,bestMatch[1]).join(''))} ${line} ${col}\n`);
                 matchers.forEach(matcher => matcher.reset());
                 tape.rewind(bytes.length - bestMatch[1]);
@@ -53,10 +50,6 @@
             //     col=1;
             // }
         }
-        console.log('Got to end of file...');
-        console.log(bytes)
-        console.log(matchers.map(x=>`${x.getType().name}:${x.state}`).join(', '))
-        console.log(bestMatch)
     } catch(e) {
         console.error(e);
         system.exit(1);
