63e3726bb1194e18c3144f62a9e207c3622cceba
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2024-03-04 16:24:45 -0700

    Update index.ts

--- CFG/src/index.ts
+++ CFG/src/index.ts
@@ -2,7 +2,7 @@
 ///#include "lib/io.ts"
 (function() {
     const cfg = readCFG('data/challenge.cfg');
-/*
+
     console.log('Non-Terminals:');
     console.log([...cfg.rules.keys()].map(x=>`'${x}'`).join(', '));
     console.log();
@@ -32,7 +32,7 @@
     console.log('First Sets:');
     [...cfg.rules.keys()].forEach(x=>console.log(`'${x}': {${[...firstSet(cfg,[x])[0].values()].map(x=>`'${x}'`)}}`));
     console.log();
-*/
+
     console.log('Follow Sets:');
     [...cfg.rules.keys()].forEach(x=>console.log(`'${x}': {${[...followSet(cfg,x)[0].values()].map(x=>`'${x}'`)}}`));
     console.log();
