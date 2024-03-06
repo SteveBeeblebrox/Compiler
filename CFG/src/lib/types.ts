eaab7f067c48e84f12e9839ab6e0eb03c756dd12
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2024-03-05 21:12:11 -0700

    Update types.ts

--- CFG/src/lib/types.ts
+++ CFG/src/lib/types.ts
@@ -1,6 +1,6 @@
 ///#pragma once
-namespace OpaqueTypes {
-    declare const type: unique symbol;
+declare namespace OpaqueTypes {
+    const type: unique symbol;
     export type Opaque<T,Ident> = T & {[type]:Ident};
 }
 type Opaque<T,Ident> = OpaqueTypes.Opaque<T,Ident>;
