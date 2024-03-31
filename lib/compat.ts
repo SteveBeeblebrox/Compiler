a1a191d7c95b893cfc4caccac57d9395004d9736
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2024-03-31 00:45:04 -0600

    Unify compat.ts for sjs and node

--- lib/compat.ts
+++ lib/compat.ts
@@ -1,30 +1,81 @@
 ///#pragma once
 ///#define strc(x) #x
-//@ts-nocheck
-declare function require(arg: string);
-fs = require('fs');
-declare var fs;
 
-const system = {
+declare interface SymbolConstructor {
+    readonly dispose: unique symbol,
+    readonly asyncDispose: unique symbol,
+}
+
+//@ts-expect-error
+Symbol['dispose'] ??= Symbol('dispose');
+//@ts-expect-error
+Symbol['asyncDispose'] ??= Symbol('asyncDispose');
+
+declare var system: {
+    args: string[];
+    exit(status: number): never;
+    readTextFileSync(path: string): string;
+    writeTextFileSync(path: string, data: string);
+    createTextFileReadStream(path: string): Promise<{read(bytes: number): string, [Symbol.dispose]():void}>;
+    createTextFileWriteStream(path: string): Promise<{write(text: string): void, [Symbol.dispose]():void}>;
+};
+
+globalThis['system'] ??= {
+    //@ts-expect-error
     args: process.argv.slice(1),
+    //@ts-expect-error
     exit: process.exit as (status?:number)=>never,
-    readFile(path: string): string {
-        return fs.readFileSync(path, {encoding:'utf8'});
+    readTextFileSync(path: string): string {
+        //@ts-expect-error
+        return require('fs').readFileSync(path, {encoding:'utf8'});
     },
-    writeFile(path: string, data: string) {
-        fs.writeFileSync(path, data, {encoding:'utf8'});
+    writeTextFileSync(path: string, data: string) {
+        //@ts-expect-error
+        require('fs').writeFileSync(path, data, {encoding:'utf8'});
     },
-    async createFileReadStream(path: string) {
-        const stream = fs.createReadStream(path, {encoding:'utf8'});
+    async createTextFileReadStream(path: string) {
+        //@ts-expect-error
+        const stream = require('fs').createReadStream(path, {encoding:'utf8'});
         void await new Promise(function(resolve,reject) {
             stream.on('readable',resolve);
             stream.on('error',reject);
         });
+        stream[Symbol.dispose] = ()=>{};
         return stream;
     },
-    async createFileWriteStream(path: string) {
-        const stream = fs.createWriteStream(path, {encoding:'utf8'});
+    async createTextFileWriteStream(path: string) {
+        //@ts-expect-error
+        const stream = require('fs').createWriteStream(path, {encoding:'utf8'});
+        stream[Symbol.dispose] = ()=>{};
         return stream;
+    }
+}
+
+system['createTextFileReadStream'] ??= async function createTextFileReadStream(path: string) {
+    //@ts-expect-error
+    const file = system.openSync(path, {read: true});
+    return {
+        read(bytes: number): string {
+            const buf = new Uint8Array(bytes);
+            file.readSync(buf);
+            return new TextDecoder().decode(buf);
+        },
+        [Symbol.dispose]() {
+            file.close();
+        }
+    }
+}
+
+system['createTextFileWriteStream'] ??= async function createTextFileWriteStream(path: string) {
+    //@ts-expect-error
+    const file = system.openSync(path, {write: true, create: true});
+    return {
+        write(text: string) {
+            file.writeSync(new TextEncoder().encode(text));
+        },
+        [Symbol.dispose]() {
+            file.close();
+        }
     }
 }
 
