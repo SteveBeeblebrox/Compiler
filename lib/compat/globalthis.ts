c582c63a0b719fc99f342a6aa43218c809c67feb
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2024-04-13 18:40:16 -0600

    Update globalthis.ts

--- lib/compat/globalthis.ts
+++ lib/compat/globalthis.ts
@@ -1,30 +1,14 @@
 ///#pragma once
-((() => {
-    if (typeof globalThis === "object") return;
-    try {
-        Object.defineProperty(Object.prototype, "__magic__", {
-            get() {
-                return this;
-            },
-            configurable: true
-        });
-        //@ts-ignore
-        __magic__.globalThis = __magic__;
-        // The previous line should have made `globalThis` globally
-        // available, but it fails in Internet Explorer 10 and older.
-        // Detect this failure and fall back.
-        if (typeof globalThis === "undefined") {
-            // Assume `window` exists.
-            //@ts-ignore
-            window.globalThis = window;
-        }
-        //@ts-ignore
-        delete Object.prototype.__magic__;
-    }
-    catch (error) {
-        // In IE8, Object.defineProperty only works on DOM objects.
-        // If we hit this code path, assume `window` exists.
-        //@ts-ignore
-        window.globalThis = window;
-    }
-})());
+//@ts-nocheck
+
+(function() {
+	if (typeof globalThis === 'object') return;
+	Object.defineProperty(Object.prototype, '__magic__', {
+		get: function() {
+			return this;
+		},
+		configurable: true
+	});
+	__magic__.globalThis = __magic__;
+	delete Object.prototype.__magic__;
+}());
