5ba1dda8e84b6e09ecb8b8a701f040fc4fe74428
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2024-04-13 18:40:23 -0600

    Update object.ts

--- lib/compat/object.ts
+++ lib/compat/object.ts
@@ -1,8 +1,17 @@
-declare interface Object {
-    groupBy<T,Key extends PropertyKey>(values: T[], callback: (element: T,index: number)=>Key): {[key in Key]: T[]};
-}
+declare interface Object {}
 
 namespace ObjectPolyfill {
+    void 0;
+}
+
+installPolyfill(Object,ObjectPolyfill);
+
+declare interface ObjectConstructor {
+    groupBy<T,Key extends PropertyKey>(values: T[], callback: (element: T,index: number)=>Key): {[key in Key]: T[]};
+    hasOwn(o: object, v: PropertyKey): boolean;
+}
+
+namespace ObjectConstructorPolyfill {
     export function groupBy<T,Key extends PropertyKey>(values: T[], callback: (element: T,index: number)=>Key): {[key in Key]: T[]} {
         return values.reduce((obj, value, index) => {
             const key = callback(value,index);
@@ -10,6 +19,9 @@ namespace ObjectPolyfill {
             return obj;
         }, {} as {[key in Key]: T[]});
     }
+    export function hasOwn(o: object, v: PropertyKey): boolean {
+        return Object.prototype.hasOwnProperty.call(o,v);
+    }
 }
 
-installPolyfill(Object,ObjectPolyfill);
+installPolyfill({prototype: Object} as any, ObjectConstructorPolyfill);
