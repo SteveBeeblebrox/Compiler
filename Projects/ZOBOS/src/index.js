37af8591a32458bccfbb56151ef6be581d05244e
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2024-05-03 14:29:26 -0600

    Update

--- Projects/ZOBOS/src/index.js
+++ Projects/ZOBOS/src/index.js
@@ -3145,7 +3145,7 @@ var ZLang;
                 V.add(node.header);
                 V.add(node.rvar);
                 for (const p of node.header.parameters) {
-                    node.scope.declare(name, p.type.ztype, p.pos, { initialized: true });
+                    node.scope.declare(p.ident.name, p.type.ztype, p.pos, { initialized: true });
                 }
                 node.scope.declare(node.rvar.name, node.header.rtype.ztype, node.rvar.pos, { initialized: true });
             }
