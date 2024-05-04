6b3e4b4078a4310ef336465026756967c3d6a846
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2024-05-03 18:25:20 -0600

    Cleanup

--- core/woot.ts
+++ core/woot.ts
@@ -4,34 +4,8 @@
 ///#pragma once
 
 ///#include <compat.ts>
-///#include <tape.ts>
 
-///#include "cfg.ts"
 ///#include "zlang.ts"
-///#include "scanner.ts"
-
-namespace zZLang {
-    console.log('Building Scanner...');
-    const SCANNER = Scanner.fromString(new BasicTextDecoder().decode(new Uint8Array([
-        ///#embed "zlang.lut"
-    ])), 'zlex.json.lz');
-    console.log('Done!');
-
-    export async function* tokenizeFile(file: string) {
-        const inStream = await system.createTextFileReadStream(file);
-        yield* SCANNER.tokenize((function*() {
-            while(true) yield inStream.read(1) as char;
-        })());
-    }
-
-    export function tokenize(text: string) {
-        return SCANNER.tokenize(text.split('')[Symbol.iterator]());
-    }
-
-    export function parse(text: string): ZLang.Program {
-        return ZLang.parseTokens(ZLang.tokenize(text)) as ZLang.Program;
-    }
-}
 
 ///#if __MAIN__
 console.log('Tokens:')
