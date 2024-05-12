bdc779b1fe53753bcc1dc86ffd728ce2f9926a75
Trin Wasinger <41750582+SteveBeeblebrox@users.noreply.github.com>
2024-05-12 00:25:46 -0600

    Fix

--- Projects/CZAR/src/index.js
+++ Projects/CZAR/src/index.js
@@ -4943,7 +4943,7 @@ var ZLang;
 ///<reference path="../../../lib/graphviz.ts"/> // Make VS Code happy
 ;
 (function () {
-    const [regs = '4,4', def, output = def.replace(/\.*?$/, '.czr')] = system.args.slice(1);
+    const [regs = '4,4', def, output = def.replace(/\..*?$/, '.czr')] = system.args.slice(1);
     const [RN, RF] = regs.split(',').map(x => +x);
     // Validate that .def file is readable
     void system.readTextFileSync(def);
--- Projects/CZAR/src/index.ts
+++ Projects/CZAR/src/index.ts
@@ -12,7 +12,7 @@
 ///<reference path="../../../lib/graphviz.ts"/> // Make VS Code happy
 
 ;(function() {
-    const [regs='4,4',def,output=def.replace(/\.*?$/,'.czr')] = system.args.slice(1);
+    const [regs='4,4',def,output=def.replace(/\..*?$/,'.czr')] = system.args.slice(1);
     const [RN,RF] = regs.split(',').map(x=>+x);
 
     // Validate that .def file is readable
